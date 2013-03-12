3391266d58d96fcbccea35b82dedf51506ad364c
mariusmuntean <marius.muntean@live.com>
2013-03-12 17:34:48 +0100

    1. Temporarily switched back to a synchronous implementation. 2. Added a basic version of findItems 3. Changed dype of MediaItem's "type" field from an Enum to String, as described here: http://dev.w3.org/2006/webapi/WebIDL/java.html#java-enumeration

--- webinos/platform/android/impl/src/org/webinos/impl/mediacontent/MediaSourceImpl.java
+++ webinos/platform/android/impl/src/org/webinos/impl/mediacontent/MediaSourceImpl.java
@@ -19,11 +19,16 @@
 package org.webinos.impl.mediacontent;
 
 import java.io.File;
+import java.io.StreamCorruptedException;
 import java.security.acl.LastOwnerException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.UUID;
 
+import org.meshpoint.anode.AndroidContext;
 import org.meshpoint.anode.module.IModule;
 import org.meshpoint.anode.module.IModuleContext;
 import org.webinos.api.DeviceAPIError;
@@ -35,19 +40,26 @@ import org.webinos.api.mediacontent.MediaFolderArraySuccessCallback;
 import org.webinos.api.mediacontent.MediaFolderStorageType;
 import org.webinos.api.mediacontent.MediaItem;
 import org.webinos.api.mediacontent.MediaItemArraySuccessCallback;
+import org.webinos.api.mediacontent.MediaItemType;
 import org.webinos.api.mediacontent.MediaSource;
 import org.webinos.api.mediacontent.PendingFindOperation;
 import org.webinos.api.mediacontent.PendingGetOperation;
 import org.webinos.api.mediacontent.PendingUpdateOperation;
 import org.webinos.api.mediacontent.SortMode;
 
+import android.R.string;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.util.Log;
 
 public class MediaSourceImpl extends MediaSource implements IModule {
 
 	private static final String TAG = "org.webinos.impl.MediaContent.MediaSourceImpl";
+	private Context androidContext;
 
 	/**
 	 * IModule methods
@@ -55,6 +67,7 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 	@Override
 	public Object startModule(IModuleContext ctx) {
 		// TODO Auto-generated method stub
+		this.androidContext = ((AndroidContext) ctx).getAndroidContext();
 		return this;
 	}
 
@@ -77,12 +90,6 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 	}
 
 	@Override
-	public PendingUpdateOperation updateItemsBatch(MediaItem[] items, SuccessCallback successCallback) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public PendingUpdateOperation updateItemsBatch(MediaItem[] items, SuccessCallback successCallback,
 			ErrorCallback errorCallback) {
 		// TODO Auto-generated method stub
@@ -92,70 +99,153 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 	@Override
 	public PendingGetOperation getFolders(MediaFolderArraySuccessCallback successCallback,
 			ErrorCallback errorCallback) {
-		Thread c = Thread.currentThread();
-		Log.i(TAG,"Thread ID from getFolders: "+c.getId());
-		GetMediaFoldersRunnable pgot = new GetMediaFoldersRunnable(successCallback, errorCallback);
-		pgot.execute(successCallback);
-		return new PendingGetOperationThreaded(pgot);
+		/*
+		 * Thread c = Thread.currentThread(); Log.i(TAG, "Thread ID from getFolders: " + c.getId()); GetMediaFoldersRunnable pgot = new GetMediaFoldersRunnable(successCallback,
+		 * errorCallback);
+		 * 
+		 * PendingGetOperationThreaded pgo = new PendingGetOperationThreaded(pgot);
+		 * 
+		 * pgot.execute(null);
+		 * 
+		 * Thread current = Thread.currentThread();
+		 */
+
+		this.SendMediaFolders(successCallback, errorCallback);
+		return null;
 	}
 
 	@Override
 	public PendingFindOperation findItems(MediaItemArraySuccessCallback successCallback,
 			ErrorCallback errorCallback, String folderId, AbstractFilter filter, SortMode sortMode,
 			long count, long offset) {
-		// TODO Auto-generated method stub
+		/**
+		 * FindItemsRunnable fir = new FindItemsRunnable(successCallback, errorCallback);
+		 * 
+		 * PendingFindOperationThreaded pfo = new PendingFindOperationThreaded(fir);
+		 * 
+		 * fir.execute(null);
+		 */
+		this.SendMediaItems(successCallback, errorCallback);
 		return null;
 	}
 
-	/**
-	 * Test methods
+	/*
+	 * Private Methods
 	 */
-	@Override
-	public String TestMethod() {
-		// TODO Auto-generated method stub
-		return "Test method returned succesfully!";
+
+	/**
+	 * 
+	 * @param file
+	 * @return A MediaFolder instance representing the physical folder from the parameter
+	 */
+	private MediaFolder mediaFolderFromFile(File file) {
+		// Even if the call to Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+		// actually returns something, check if the folder really exists, I had to find it out the hard way.
+		if (!file.exists()) {
+			try {
+				throw new Exception("File/Folder does not exist!");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		MediaFolder mf = new MediaFolder();
+		try {
+			mf.id = UUID.nameUUIDFromBytes(file.getName().getBytes()).toString();
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+			mf.id = "error";
+		}
+		mf.folderURI = file.getPath();
+		mf.title = file.getName();
+		mf.storageType = MediaFolderStorageType.INTERNAL;
+		// Careful
+		String d = new String().valueOf(file.lastModified());
+		long newFileDateMillis = (Long.parseLong(d) / 1000) * 1000;
+		mf.modifiedDate = new Date(newFileDateMillis);
+
+		Log.i(TAG, "MediaFolder: " + mf.title + " @:" + mf.folderURI + " ID: " + mf.id + " on:"
+				+ mf.modifiedDate.toLocaleString() + " Type: " + mf.storageType + " TypeInt: "
+				+ mf.storageType.displayintegerValue());
+		return mf;
 	}
 
-	/**
-	 * Thread implementations of some Pending-Find/Get/Update-Operation
-	 */
-	/*
-	 * class GetMediaFoldersRunnable implements ICancelableRunnable {
-	 * 
-	 * MediaFolderArraySuccessCallback mfasCallback; ErrorCallback errorCallback; Boolean canceled = false;
-	 * 
-	 * private GetMediaFoldersRunnable(MediaFolderArraySuccessCallback mfasCallback, ErrorCallback eCallback) { this.mfasCallback = mfasCallback; this.errorCallback = eCallback; }
-	 * 
-	 * @Override public void run() { try { ArrayList<File> mediaFolders = new ArrayList<File>(); mediaFolders.add(Environment
-	 * .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)); mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
-	 * mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)); if (this.canceled) return; MediaFolder[] resultArray = new
-	 * MediaFolder[mediaFolders.size()]; for (int i = 0; i < mediaFolders.size(); i++) { try { resultArray[i] = mediaFolderFromFile(mediaFolders.get(i)); } catch (Exception e) {
-	 * this.errorCallback.onerror(new DeviceAPIError(DeviceAPIError.SECURITY_ERR)); } if (this.canceled) return; }
-	 * 
-	 * this.mfasCallback.onsuccess(resultArray);
-	 * 
-	 * } catch (Exception e) { this.errorCallback.onerror(new DeviceAPIError(DeviceAPIError.NOT_SUPPORTED_ERR)); } }
-	 * 
-	 * private MediaFolder mediaFolderFromFile(File file) { // Even if the call to Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC); // actually returns
-	 * something, check if the folder really exists, I had to find it out the hard way. if (!file.exists()) { try { throw new Exception("File/Folder does not exist!"); } catch
-	 * (Exception e) { // TODO Auto-generated catch block e.printStackTrace(); } }
-	 * 
-	 * MediaFolder mf = new MediaFolder(); mf.id = UUID.randomUUID().toString(); mf.folderURI = file.getPath(); mf.title = file.getName(); mf.storageType =
-	 * MediaFolderStorageType.INTERNAL; // Careful, if the folder doesn't contain anything (like when you have a new phone) the last modified // date can cause problems, just check
-	 * if the folder actually exists(regardless if Android gives you // the file object to it) and maybe if it is empty or not. long lastModified = file.lastModified();
-	 * mf.modifiedDate = new Date(lastModified);
-	 * 
-	 * Log.i(TAG, "MediaFolder: " + mf.title + " @:" + mf.folderURI + " on:" + mf.modifiedDate.toString()); return mf; }
-	 * 
-	 * 
-	 * @Override public void cancel() { Thread currentThread = Thread.currentThread(); currentThread.interrupt(); this.canceled = true;
-	 * 
-	 * }
-	 * 
-	 * }
-	 */
+	private void SendMediaFolders(MediaFolderArraySuccessCallback successCallback, ErrorCallback errorCallback) {
+		try {
+			ArrayList<File> mediaFolders = new ArrayList<File>();
+			mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+			mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
+			mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES));
 
-	class GetMediaFoldersRunnable extends AsyncTask<MediaFolderArraySuccessCallback, Integer, MediaFolder[]>
+			MediaFolder[] resultArray = new MediaFolder[mediaFolders.size()];
+			for (int i = 0; i < mediaFolders.size(); i++) {
+				try {
+					resultArray[i] = mediaFolderFromFile(mediaFolders.get(i));
+				} catch (Exception e) {
+					errorCallback.onerror(new DeviceAPIError(DeviceAPIError.SECURITY_ERR));
+					return;
+				}
+			}
+			successCallback.onsuccess(resultArray);
+			return;
+		} catch (Exception e) {
+			errorCallback.onerror(new DeviceAPIError(DeviceAPIError.NOT_SUPPORTED_ERR));
+			Log.i(TAG, "Failed while getting media folders with following error:" + e.toString());
+			return;
+		}
+	}
+
+	private void SendMediaItems(MediaItemArraySuccessCallback successCallback, ErrorCallback errorCallback) {
+		try {
+			ArrayList<MediaItem> mediaItemsList = new ArrayList<MediaItem>();
+			String[] projection = {};
+			String selection = "";
+			String[] selectionArgs = {};
+			String sortOrder = "";
+
+			Cursor cursor = androidContext.getContentResolver().query(
+					MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs,
+					sortOrder);
+
+			if (!cursor.moveToFirst()) {
+				return;
+			}
+
+			while (cursor.moveToNext()) {
+				MediaItem mi = new MediaItem();
+				mi.editableAttributes = new String[]{"title"};
+				mi.id = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media._ID));
+				mi.title = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.TITLE));
+				mi.itemURI = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+				String strMillis = String.valueOf((cursor.getLong(cursor
+						.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN))));
+				long longMillis = (Long.parseLong(strMillis) / 1000) * 1000;
+				mi.releaseDate = new Date(longMillis);
+				mi.modifiedDate = new Date(cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.DATE_MODIFIED)));
+//				mi.modifiedDate = new Date(System.currentTimeMillis());
+				mi.mimeType = "Image";
+				mi.type = MediaItemType.IMAGE.toString();
+				mi.size = cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.SIZE));
+				String description = cursor.getString(cursor
+						.getColumnIndex(MediaStore.Images.Media.DESCRIPTION));
+				mi.description = (description == null ? "no description"
+						: (description.isEmpty() ? "no description" : description));
+				mi.rating = 7;
+
+				mediaItemsList.add(mi);
+			}
+//			cursor = null;
+
+			MediaItem[] result = new MediaItem[mediaItemsList.size()];
+			result = mediaItemsList.toArray(result);
+			successCallback.onsuccess(result);
+		} catch (Exception e) {
+			errorCallback.onerror(new DeviceAPIError(DeviceAPIError.UNKNOWN_ERR));
+		}
+	}
+
+	class GetMediaFoldersRunnable extends AsyncTask<MediaFolderArraySuccessCallback, Void, MediaFolder[]>
 			implements ICancelableRunnable {
 
 		MediaFolderArraySuccessCallback mfasCallback;
@@ -179,25 +269,34 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 			}
 
 			MediaFolder mf = new MediaFolder();
-			mf.id = UUID.randomUUID().toString();
+			try {
+				mf.id = UUID.nameUUIDFromBytes(file.getName().getBytes()).toString();
+			} catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+				mf.id = "error";
+			}
 			mf.folderURI = file.getPath();
 			mf.title = file.getName();
 			mf.storageType = MediaFolderStorageType.INTERNAL;
-			// Careful, if the folder doesn't contain anything (like when you have a new phone) the last modified
-			// date can cause problems, just check if the folder actually exists(regardless if Android gives you
-			// the file object to it) and maybe if it is empty or not.
-			long lastModified = file.lastModified();
-			mf.modifiedDate = new Date(lastModified);
+			// Careful
+			String d = new String().valueOf(file.lastModified());
+			long newFileDateMillis = (Long.parseLong(d) / 1000) * 1000;
+			mf.modifiedDate = new Date(newFileDateMillis);
 
-			Log.i(TAG,
-					"MediaFolder: " + mf.title + " @:" + mf.folderURI + " on:" + mf.modifiedDate.toString());
+			Log.i(TAG, "MediaFolder: " + mf.title + " @:" + mf.folderURI + " ID: " + mf.id + " on:"
+					+ mf.modifiedDate.toLocaleString() + " Type: " + mf.storageType + " TypeInt: "
+					+ mf.storageType.displayintegerValue());
 			return mf;
 		}
-		private MediaFolder MakeDummyMF() {
+
+		private MediaFolder MakeDummyMF(File file) {
 			MediaFolder mFolder = new MediaFolder();
 			mFolder.folderURI = "/path/to/folder";
-			mFolder.id = "12345678";
-			mFolder.modifiedDate = new Date(1988, 10, 8);
+			mFolder.id = UUID.randomUUID().toString();
+			long fileDate = file.lastModified();
+			long systemDate = System.currentTimeMillis();
+
+			mFolder.modifiedDate = new Date(systemDate);
 			mFolder.storageType = MediaFolderStorageType.EXTERNAL;
 			mFolder.title = "Marius_DummyFolder";
 
@@ -220,30 +319,30 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
 				mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
 				mediaFolders.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES));
-				if (this.isCancelled()){
-					Log.i(TAG,"Canceled");
+				if (this.isCancelled()) {
+					Log.i(TAG, "Canceled");
 					return null;
 				}
 				MediaFolder[] resultArray = new MediaFolder[mediaFolders.size()];
 				for (int i = 0; i < mediaFolders.size(); i++) {
 					try {
 						resultArray[i] = mediaFolderFromFile(mediaFolders.get(i));
-//						resultArray[i] = MakeDummyMF();
 					} catch (Exception e) {
 						this.errorCallback.onerror(new DeviceAPIError(DeviceAPIError.SECURITY_ERR));
 						return null;
 					}
-					if (this.isCancelled()){
-						Log.i(TAG,"Canceled");
+					if (this.isCancelled()) {
+						Log.i(TAG, "Canceled");
 						return null;
+					}
 				}
-				}
-				Thread c = Thread.currentThread();
-				Log.i(TAG,"Thread ID from doInBackground: "+c.getId());
+				// Thread c = Thread.currentThread();
+				// Log.i(TAG, "Thread ID from doInBackground: " + c.getId());
+				// params[0].onsuccess(resultArray);
 				return resultArray;
 			} catch (Exception e) {
 				this.errorCallback.onerror(new DeviceAPIError(DeviceAPIError.NOT_SUPPORTED_ERR));
-				Log.i(TAG,"Failed while getting media folders with following error:"+e.toString());
+				Log.i(TAG, "Failed while getting media folders with following error:" + e.toString());
 				return null;
 			}
 		}
@@ -254,9 +353,81 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 			super.onPostExecute(result);
 			// Callback only if it wasn't interrupted
 			if (result != null) {
-				Thread c = Thread.currentThread();
-				Log.i(TAG,"Thread ID from onPostExecute: "+c.getId());
+				// Thread c = Thread.currentThread();
+				// Log.i(TAG, "Thread ID from onPostExecute: " + c.getId());
 				mfasCallback.onsuccess(result);
+			}
+		}
+
+	}
+
+	class FindItemsRunnable extends AsyncTask<MediaItemArraySuccessCallback, Void, MediaItem[]> implements
+			ICancelableRunnable {
+
+		MediaItemArraySuccessCallback miasCallback;
+		ErrorCallback errorCallback;
+
+		private FindItemsRunnable(MediaItemArraySuccessCallback miasCallback, ErrorCallback errorCallback) {
+			this.miasCallback = miasCallback;
+			this.errorCallback = errorCallback;
+		}
+
+		@Override
+		public void cancelJob() {
+			Log.i(TAG, "Cancel called");
+			this.cancel(true);
+		}
+
+		@Override
+		protected MediaItem[] doInBackground(MediaItemArraySuccessCallback... params) {
+			ArrayList<MediaItem> mediaItemsList = new ArrayList<MediaItem>();
+
+			String[] projection = {};
+			String selection = "";
+			String[] selectionArgs = {};
+			String sortOrder = "";
+
+			Cursor cursor = androidContext.getContentResolver().query(
+					MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs,
+					sortOrder);
+
+			if (!cursor.moveToFirst()) {
+				return null;
+			}
+
+			while (cursor.moveToNext()) {
+				MediaItem mi = new MediaItem();
+				mi.id = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media._ID));
+				mi.title = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.TITLE));
+				mi.itemURI = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+				String strMillis = String.valueOf((cursor.getLong(cursor
+						.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN))));
+				long longMillis = (Long.parseLong(strMillis) / 1000) * 1000;
+				mi.releaseDate = new Date(longMillis);
+				// mi.modifiedDate = new Date(cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.DATE_MODIFIED)));
+				mi.mimeType = "Image";
+				mi.type = MediaItemType.IMAGE.toString();
+				mi.size = cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.SIZE));
+
+				mediaItemsList.add(mi);
+			}
+			cursor = null;
+
+			MediaItem[] result = new MediaItem[mediaItemsList.size()];
+			result = mediaItemsList.toArray(result);
+
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(MediaItem[] result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			// Callback only if it wasn't interrupted
+			if (result != null) {
+				// Thread c = Thread.currentThread();
+				// Log.i(TAG, "Thread ID from onPostExecute: " + c.getId());
+				this.miasCallback.onsuccess(result);
 			}
 		}
 
@@ -283,5 +454,23 @@ public class MediaSourceImpl extends MediaSource implements IModule {
 		}
 
 	}
-	
+
+	class PendingFindOperationThreaded extends PendingFindOperation {
+
+		ICancelableRunnable r;
+
+		public PendingFindOperationThreaded(ICancelableRunnable r) {
+			this.r = r;
+		}
+
+		@Override
+		public void cancel() {
+
+			if (r != null)
+				r.cancelJob();
+
+		}
+
+	}
+
 }
